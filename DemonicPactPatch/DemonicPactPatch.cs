0af0831f2371de083feb258fc27d8ef63c2d8b78
pdcook <qphysicscook@gmail.com>
2021-10-05 00:50:16 -0400

    v0.0.0

---
+++ DemonicPactPatch/DemonicPactPatch.cs
@@ -0,0 +1,30 @@
+﻿using BepInEx;
+using HarmonyLib;
+
+namespace DemonicPactPatch
+{
+    [BepInPlugin(ModId, ModName, "0.0.0")]
+    [BepInProcess("Rounds.exe")]
+    public class DemonicPactPatch : BaseUnityPlugin
+    {
+        private void Awake()
+        {
+            new Harmony(ModId).PatchAll();
+        }
+        private void Start()
+        {
+        }
+
+        private const string ModId = "pykess.rounds.plugins.demonicpactpatch";
+
+        private const string ModName = "Demonic Pact Patch";
+    }
+    [HarmonyPatch(typeof(Player), "FullReset")]
+    class PlayerPatchFullReset
+    {
+        private static void Prefix(Player __instance)
+        {
+            __instance.GetComponent<Holding>().holdable.GetComponent<Gun>().dontAllowAutoFire = false;
+        }
+    }
+}
